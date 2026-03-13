e370b0a0f755be2955e8d11246fbfe58772e2781
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-03-13 15:27:27 -0300

    modifico el hit 1 para que se ejecute como un comando

--- TPs/TPI/Hit1/Hit1A.py
+++ TPs/TPI/Hit1/Hit1A.py
@@ -4,37 +4,80 @@
 creando un objeto bytes en lugar de una cadena Unicode (str) normal. 
 Esto se utiliza para manipular datos binarios de bajo nivel (valores del 0 al 255), como archivos binarios o redes.'''
 
-#Hecho con IA a fiens de prueba
 
 import socket
+import sys
+
+
+MAX_PORT = 65000
+MIN_PORT = 2000
+
 
 def obtener_puerto():
-
-    puerto = int(input("Ingrese el numero de puerto del servidor (2000 < puerto < 65000): "))
+    puerto = 0
 
     while (puerto <= 2000 or puerto >= 65000):
-
-        puerto = int(input("Ingrese el numero de puerto del servidor (2000 < puerto < 65000): "))
+        try:
+            puerto = int(input("Ingrese el numero de puerto del servidor (2000 < puerto < 65000): "))
+        except ValueError:
+            print("Advertencia: el puerto debe ser un numero entero")
 
     return puerto
 
-def proceso_cliente(puerto):
 
+def puerto_valido(puerto: int) -> bool:
+    if (puerto <= MIN_PORT or puerto >= MAX_PORT):
+        return False
+
+    return True
+
+
+
+def enviar_mensaje(puerto: int, mensaje: str):
     # Crear socket TCP/IP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Conectar al servidor
-    server_address = ('localhost', puerto)
-    client_socket.connect(server_address)
-
     try:
+        # Conectar al servidor
+        server_address = ('localhost', puerto)
+        client_socket.connect(server_address)
+
         # Enviar datos
-        message = b"Hola CompuMundo, el cliente os saluda."
-        client_socket.sendall(message)
-        print("Mensaje enviado")
+        client_socket.sendall(mensaje.encode("utf-8"))
+
+    except ConnectionRefusedError:
+        print("Hubo un error al conectar al servidor, asegurate que este corriendo")
+        sys.exit(1)
+
     finally:
         client_socket.close()
 
-puerto = obtener_puerto()
 
-proceso_cliente(puerto)
+def main():
+    argc = len(sys.argv)
+    if (argc != 3):
+        print("Error: El comando esperado contiene numero de puerto y mensaje")
+        print("Ejemplo: 55555 \"Hola mundo\"")
+        sys.exit(1)
+
+    try:
+        puerto = int(sys.argv[1])
+    except ValueError:
+        print("Error: El puerto debe ser un entero")
+        sys.exit(1)
+
+    if not (puerto_valido(puerto)):
+        print("Error: Rango de puertos permitidos = ", MIN_PORT, " - ", MAX_PORT)
+        sys.exit(1)
+
+    mensaje = sys.argv[2].strip()
+    if not mensaje:
+        ("Error: El mensaje no puede estar vacio")
+        sys.exit(1)
+
+    enviar_mensaje(puerto, mensaje)
+    print("Mensaje enviado")
+    sys.exit(0)
+
+
+main()
