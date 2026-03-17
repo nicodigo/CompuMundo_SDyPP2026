99c154fad277c0b36cb15929b1eb9f09f530fdad
Mat-JA <ausquim@gmail.com>
2026-03-17 18:29:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
 #DEO GLORIA
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+
+    name="CI/CD_Test",
+    version="0.1",
+    author="Titanium, MouseKey, Nazra",
+    packages=find_packages(),
+    python_requires=">=3.10"
+    
+)
